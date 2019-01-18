f6681d71b99963bdc169b9f18f749762bda51f74
Anton Hansson <hansson@google.com>
2019-01-18 14:10:45 +0000

    merlin: Use TARGET_SCREEN_DENSITY
    This variable is used to auto-populate the ro.sf.lcd_density as
    well as selecting density bucket for resources in recovery.
    
    Bug: 118676832
    Test: adb shell getprop ro.sf.lcd_density
    Test: boot into recovery
    Change-Id: Ib383d18b61e7a2058a69ae26136aa053fdba4df2

--- system_prop.mk
+++ system_prop.mk
@@ -6,8 +6,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Display
 PRODUCT_PROPERTY_OVERRIDES += \
-    debug.hwui.use_buffer_age=false \
-    ro.sf.lcd_density=320
+    debug.hwui.use_buffer_age=false
 
 # Radio
 PRODUCT_PROPERTY_OVERRIDES += \
