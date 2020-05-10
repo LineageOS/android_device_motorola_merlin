f5bcbd8679205e97ce27b62cdc47bea70537b06a
althafvly <althafvly@gmail.com>
2020-05-10 22:06:22 +0530

    merlin: Fix safetynet
    Use safetynet passing fingerprint
    
    Change-Id: I78487723fa1796f21da1d841c79a3fbfc1abdc4c

--- lineage.mk
+++ lineage.mk
@@ -31,4 +31,4 @@ PRODUCT_RELEASE_NAME := merlin
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="merlin_retail-user 6.0.1 MPD24.107-56 30 release-keys"
 
-BUILD_FINGERPRINT := "motorola/merlin_retail/merlin:6.0.1/MPD24.107-56/30:user/release-keys"
+BUILD_FINGERPRINT := "motorola/merlin_retasia/merlin:6.0/MPD24.65-22/23:user/release-keys"
