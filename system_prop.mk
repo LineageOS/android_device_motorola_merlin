3a71da8b17048a45f8a832e72a8c832afea6b36e
jro1979oliver <jroliveira.oliveira301@gmail.com>
2020-07-02 14:21:38 +0000

    merlin: Add display props
    Change-Id: Ifb1cbb9d012f684d99c318417e14f1a4f1e59120

--- system_prop.mk
+++ system_prop.mk
@@ -17,3 +17,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.radio.plmn_name_cmp=1 \
     ro.telephony.ril.config=simactivation
 
+# SurfaceFlinger
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    ro.surface_flinger.protected_contents=true \
+    ro.surface_flinger.vsync_event_phase_offset_ns=4000000 \
+    ro.surface_flinger.vsync_sf_event_phase_offset_ns=8000000 \
+    ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
+    ro.surface_flinger.max_virtual_display_dimension=2048
